9b9185cbdde6e3c7cec012d56077c250c40e3de5
Jaimin308 <pateljaimin2204@gmail.com>
2025-09-07 21:33:31 +0530

    Multiply Two Numbers

---
+++ Multiply.c
@@ -0,0 +1,11 @@
+// Multiply Two Numbers
+
+#include <stdio.h>
+int main() {
+    int a, b, mul;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    mul = a * b;
+    printf("Multiplication = %d\n", mul);
+    return 0;
+}
